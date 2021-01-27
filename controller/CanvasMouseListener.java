b6ccdc9969da54866683fda727ae5fc1700badbd
Wasin Sanguansuk <wasin_sanguansuk@hotmail.com>
2021-01-27 00:16:15 -0600

    Implemented MouseListener for drawing Rectangles

--- controller/CanvasMouseListener.java
+++ controller/CanvasMouseListener.java
@@ -1,9 +1,10 @@
 package controller;
 
-import view.interfaces.PaintCanvasBase;
-
+import java.awt.*;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
+
+import view.interfaces.PaintCanvasBase;
 
 public class CanvasMouseListener implements MouseListener{
     private PaintCanvasBase paintCanvas;
@@ -12,31 +13,56 @@ public class CanvasMouseListener implements MouseListener{
 
     public CanvasMouseListener(PaintCanvasBase _paintCanvas) {
         this.paintCanvas = _paintCanvas;
+        pointPressed = new Point(0,0);
+        pointReleased = new Point(0,0);
     }
 
     public void mouseClicked(MouseEvent e) {
-
+        // Nothing
     }
 
     public void mousePressed(MouseEvent e) {
         pointPressed.setX(e.getX());
         pointPressed.setY(e.getY());
-
         System.out.println("Pressed: " + pointPressed.getX() + ", " + pointPressed.getY());
     }
 
     public void mouseReleased(MouseEvent e) {
         pointReleased.setX(e.getX());
         pointReleased.setY(e.getY());
-
         System.out.println("Released: " + pointReleased.getX() + ", " + pointReleased.getY());
+
+        Point pointTopLeft = new Point(0,0);
+
+        if (pointPressed.getX() < pointReleased.getX()) {
+            pointTopLeft.setX(pointPressed.getX());
+        } else {
+            pointTopLeft.setX(pointReleased.getX());
+        }
+
+        if (pointPressed.getY() < pointReleased.getY()) {
+            pointTopLeft.setY(pointPressed.getY());
+        } else {
+            pointTopLeft.setY(pointReleased.getY());
+        }
+
+        Graphics2D graphics2D = this.paintCanvas.getGraphics2D();
+        int width = Math.abs(pointReleased.getX() - pointPressed.getX());
+        int height = Math.abs(pointReleased.getY() - pointPressed.getY());
+        graphics2D.setColor(Color.GREEN);
+        graphics2D.fillRect(pointTopLeft.getX(), pointTopLeft.getY(), width, height);
+
+        pointPressed.setX(0);
+        pointPressed.setY(0);
+        pointReleased.setX(0);
+        pointReleased.setY(0);
     }
 
     public void mouseEntered(MouseEvent e) {
-
+        // Nothing
     }
 
     public void mouseExited(MouseEvent e) {
-
+        // Nothing
     }
 }
--- main/Main.java
+++ main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import controller.CanvasMouseListener;
 import controller.IJPaintController;
 import controller.JPaintController;
 import model.ShapeType;
@@ -15,6 +16,7 @@ import java.awt.*;
 
 public class Main {
 	public static void main(String[] args) {
+		// Setup
 		PaintCanvasBase paintCanvas = new PaintCanvas();
 		IGuiWindow guiWindow = new GuiWindow(paintCanvas);
 		IUiModule uiModule = new Gui(guiWindow);
@@ -22,50 +24,54 @@ public class Main {
 		IJPaintController controller = new JPaintController(uiModule, appState);
 		controller.setup();
 
+		// Mouse Listener
+		CanvasMouseListener mouseListener = new CanvasMouseListener(paintCanvas);
+		paintCanvas.addMouseListener(mouseListener);
+		
 		
 
-		
-		
-		
+
+
+
+
+
 
 		//paintCanvas.addMouseListener(new );
 		
-		ShapeType shapeType = appState.getActiveShapeType();
+		//ShapeType shapeType = appState.getActiveShapeType();
 
-		if (shapeType == ShapeType.RECTANGLE) {
+		//if (shapeType == ShapeType.RECTANGLE) {
 
-		}
+		//}
 
-		
-		
-		
+
 		
 		// For example purposes only; remove all lines below from your final project.
 
-		try {
-			Thread.sleep(500);
-			//Thread.sleep(1500);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		//try {
+		//	Thread.sleep(500);
+		//	//Thread.sleep(1500);
+		//} catch (InterruptedException e) {
+		//	e.printStackTrace();
+		//}
 
 		// Filled in rectangle
-		Graphics2D graphics2d = paintCanvas.getGraphics2D();
-		graphics2d.setColor(Color.GREEN);
-		graphics2d.fillRect(12, 13, 200, 400);
+		//Graphics2D graphics2d = paintCanvas.getGraphics2D();
+		//graphics2d.setColor(Color.GREEN);
+		//graphics2d.fillRect(12, 13, 200, 400);
 		//graphics2d.fillRect(42, 43, 200, 600);
 
 		// Outlined rectangle
-		graphics2d.setStroke(new BasicStroke(5));
+		//graphics2d.setStroke(new BasicStroke(5));
 		//graphics2d.setStroke(new BasicStroke(8));
-		graphics2d.setColor(Color.BLUE);
-		graphics2d.drawRect(12, 13, 200, 400);
+		//graphics2d.setColor(Color.BLUE);
+		//graphics2d.drawRect(12, 13, 200, 400);
 
 		// Selected Shape
-		Stroke stroke = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 1, new float[] { 9 }, 0);
+		//Stroke stroke = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 1, new float[] { 9 }, 0);
 		//Stroke stroke = new BasicStroke(6, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER, 4, new float[] { 15 }, 0);
-		graphics2d.setStroke(stroke);
-		graphics2d.setColor(Color.BLACK);
-		graphics2d.drawRect(7, 8, 210, 410);
+		//graphics2d.setStroke(stroke);
+		//graphics2d.setColor(Color.BLACK);
+		//graphics2d.drawRect(7, 8, 210, 410);
 	}
 }
